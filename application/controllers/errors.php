e00eb4513d8b5836a3f6c7925c791b06db614740
jamesgiang <james.l.giang@gmail.com>
2012-03-04 21:53:07 +1100

    fixed up error controller

--- application/controllers/errors.php
+++ application/controllers/errors.php
@@ -3,14 +3,10 @@
 class Errors extends CI_Controller {
 
 	public function not_found() {
-<<<<<<< HEAD
 		$data['page_title'] = 'Page not Found';
 		$data['css_file'] = '404';
 		$data['main_content'] = 'errors/404';
 		$this->load->view('includes/template',$data);
-=======
-		$this->load->view('errors/404');
->>>>>>> e1e0c62eb3ff9c00f2f770e503d509653a736500
 	}
 }
 
