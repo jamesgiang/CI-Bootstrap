0eb8ebed82621d5e3375e12577000fb449fdb958
jamesgiang <james.l.giang@gmail.com>
2012-03-04 18:23:11 +1100

    added htaccess and basic config settings

--- application/config/config_template.php
+++ application/config/config_template.php
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -279,7 +279,7 @@ $config['cookie_secure']	= FALSE;
 | COOKIE data is encountered
 |
 */
-$config['global_xss_filtering'] = FALSE;
+$config['global_xss_filtering'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
