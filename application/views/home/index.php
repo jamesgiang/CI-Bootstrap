15a7b8b7495bd619728dd25b609297e7d846eefd
jamesgiang <james.l.giang@gmail.com>
2012-03-04 20:17:55 +1100

    tab indented <head> section

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,45 +1,44 @@
 <?php echo doctype('html5'); ?>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>CI Bootstrap</title>
-    <?php echo meta('viewport', 'width=device-width, initial-scale=1.0'); ?>
-    <?php echo meta('description', ''); ?>
-    <?php echo meta('author', ''); ?>
+	<head>
+		<meta charset="utf-8">
+		<title>CI Bootstrap</title>
+		<?php echo meta('viewport', 'width=device-width, initial-scale=1.0'); ?>
+		<?php echo meta('description', ''); ?>
+		<?php echo meta('author', ''); ?>
+		
+		<?php echo link_tag('assets/css/bootstrap.css'); ?>
+		<?php echo link_tag('assets/css/app.css'); ?>
+		<?php echo link_tag('assets/css/bootstrap-responsive.css'); ?>
 
-    <!-- Le styles -->
-	<?php echo link_tag('assets/css/bootstrap.css'); ?>
-	<?php echo link_tag('assets/css/bootstrap-responsive.css'); ?>
-	<?php echo link_tag('assets/css/app.css'); ?>
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
 
-    <!--[if lt IE 9]>
-      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-    <?php
-    	$link = array(	'rel' => 'shortcut icon',
-						'href' => 'assets/img/favicon.ico');
-		echo link_tag($link);
-		$link = array(	'rel' => 'apple-touch-icon',
-						'href' => 'assets/img/apple-touch-icon.png');
-		echo link_tag($link);
-		$link = array(	'rel' => 'apple-touch-icon',
-						'href' => 'assets/img/apple-touch-icon-precomposed.png');
-		echo link_tag($link);
-		$link = array(	'rel' => 'apple-touch-icon',
-						'href' => 'assets/img/apple-touch-icon-57x57-precomposed.png',
-						'sizes' => '57x57');
-		echo link_tag($link);
-		$link = array(	'rel' => 'apple-touch-icon',
-						'href' => 'assets/img/apple-touch-icon-72x72-precomposed.png',
-						'sizes' => '72x72');
-		echo link_tag($link);
-		$link = array(	'rel' => 'apple-touch-icon',
-						'href' => 'assets/img/apple-touch-icon-114x114-precomposed.png',
-						'sizes' => '114x114');
-		echo link_tag($link);
-	?>
-  </head>
+		<?php
+			$link = array(	'rel' => 'shortcut icon',
+							'href' => 'assets/img/favicon.ico');
+			echo link_tag($link);
+			$link = array(	'rel' => 'apple-touch-icon',
+							'href' => 'assets/img/apple-touch-icon.png');
+			echo link_tag($link);
+			$link = array(	'rel' => 'apple-touch-icon',
+							'href' => 'assets/img/apple-touch-icon-precomposed.png');
+			echo link_tag($link);
+			$link = array(	'rel' => 'apple-touch-icon',
+							'href' => 'assets/img/apple-touch-icon-57x57-precomposed.png',
+							'sizes' => '57x57');
+			echo link_tag($link);
+			$link = array(	'rel' => 'apple-touch-icon',
+							'href' => 'assets/img/apple-touch-icon-72x72-precomposed.png',
+							'sizes' => '72x72');
+			echo link_tag($link);
+			$link = array(	'rel' => 'apple-touch-icon',
+							'href' => 'assets/img/apple-touch-icon-114x114-precomposed.png',
+							'sizes' => '114x114');
+			echo link_tag($link);
+		?>
+	</head>
 
   <body>
 
