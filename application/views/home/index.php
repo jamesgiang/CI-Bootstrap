b3304aa2ad4e5289458cc1508001a59f31194f1d
jamesgiang <james.l.giang@gmail.com>
2012-03-04 20:24:14 +1100

    loading scripts via CI + google analytics

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -40,7 +40,7 @@
 		?>
 	</head>
 
-  <body>
+	<body>
 
     <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
@@ -138,22 +138,14 @@
 
     </div><!--/.fluid-container-->
     
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../assets/js/jquery.js"></script>
-    <script src="../assets/js/bootstrap-transition.js"></script>
-    <script src="../assets/js/bootstrap-alert.js"></script>
-    <script src="../assets/js/bootstrap-modal.js"></script>
-    <script src="../assets/js/bootstrap-dropdown.js"></script>
-    <script src="../assets/js/bootstrap-scrollspy.js"></script>
-    <script src="../assets/js/bootstrap-tab.js"></script>
-    <script src="../assets/js/bootstrap-tooltip.js"></script>
-    <script src="../assets/js/bootstrap-popover.js"></script>
-    <script src="../assets/js/bootstrap-button.js"></script>
-    <script src="../assets/js/bootstrap-collapse.js"></script>
-    <script src="../assets/js/bootstrap-carousel.js"></script>
-    <script src="../assets/js/bootstrap-typeahead.js"></script>
-
-  </body>
+		<script src="<?php echo site_url(); ?>assets/js/jquery.js"></script>
+		<script src="<?php echo site_url(); ?>assets/js/bootstrap.js"></script>
+		<script src="<?php echo site_url(); ?>assets/js/app.js"></script>
+		<script>
+			var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+	</body>
 </html>
