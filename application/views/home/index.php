113a77315b41870263fa61e087192b0571d10f3d
jamesgiang <james.l.giang@gmail.com>
2012-03-04 19:55:53 +1100

    loading meta data via CI

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Bootstrap, from Twitter</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <?php echo meta('viewport', 'width=device-width, initial-scale=1.0'); ?>
+    <?php echo meta('description', ''); ?>
+    <?php echo meta('author', ''); ?>
 
     <!-- Le styles -->
 	<?php echo link_tag('assets/css/bootstrap.css'); ?>
